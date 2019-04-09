4b812c8908fec223f6dcd5e4775a431c381849a3
veghp <31794642+veghp@users.noreply.github.com>
2019-04-09 17:26:43 +0100

    Export specified cell types

--- export_cellphonedb.R
+++ export_cellphonedb.R
@@ -1,6 +1,7 @@
 # Set parameters, then source file.
-path.seuratobject <- "" # character string. Path to Seurat object file
-annotation <- "" # Seuratobject metadata column name
+path.seuratobject <- "" # string. Path to Seurat object file
+annotation <- "" # string. Seuratobject metadata column name
+celltypes <- NULL # string vector. Export cells of these annotation labels. If NULL, export all cells in metadata.
 metadata.filename <- "" # metadata export filename
 count.filename <- "" # count export filename
 gene.csv <- "" # path to gene.csv downloaded from https://www.cellphonedb.org/downloads/gene.csv
@@ -9,12 +10,19 @@ library(Seurat)
 library(plyr)
 seuratobject <- readRDS(path.seuratobject)
 seuratobject <- SetAllIdent(seuratobject, id = annotation)
-if(!all(dim(seuratobject@data) == dim(seuratobject@raw.data))) { stop("Raw and processed data dimensions are not the same.") }
+if(!all(dim(seuratobject@data) == dim(seuratobject@raw.data))) { warning("Raw and processed data dimensions are not the same. Raw data will be subset for the cells in metadata.") }
 
 # Export metadata for CellPhoneDB:
 ##################################
 metadata <- FetchData(seuratobject, vars.all = annotation)
-metadata <- data.frame(Cell = rownames(metadata), cell_type = metadata[, annotation]) # string "cell_type" is required by CellPhoneDB
+# Get cell names
+if(is.null(celltypes)) {
+  cells <- rownames(metadata)
+} else {
+  cells <- WhichCells(seuratobject, ident = celltypes)
+}
+
+metadata <- data.frame(Cell = cells, cell_type = metadata[cells, annotation]) # string "cell_type" is required by CellPhoneDB
 write.table(metadata, file = metadata.filename, row.names = FALSE, quote = F, sep = "\t")
 
 # Export count data for CellPhoneDB:
@@ -22,7 +30,8 @@ write.table(metadata, file = metadata.filename, row.names = FALSE, quote = F, se
 # Load CellPhoneDB gene list:
 cellphonedb.genes <- read.csv(gene.csv, stringsAsFactors = F)
 # Get gene expression data:
-counts <- as.matrix(seuratobject@raw.data)
+counts <- seuratobject@raw.data[, cells]
+counts <- as.matrix(counts)
 genes <- unique(cellphonedb.genes$hgnc_symbol[cellphonedb.genes$hgnc_symbol %in% rownames(counts)])
 # Replace data rownames (HGNC symbols) with ENS ids (CellPhoneDB uses ENS ids):
 # One HGNC symbol can have multiple ENS ids, but it will be implicitly handled, i.e. first value will be used.
@@ -32,7 +41,7 @@ ens.rownames <- mapvalues(rownames(counts),
 
 #grep("ENS", ens.rownames, value = T) # check
 
-# Count normalization
+# Count normalization
 column.sums <- colSums(counts)
 cpm <- sweep(counts, 2, column.sums, "/")
 cpm <- cpm * 10000
