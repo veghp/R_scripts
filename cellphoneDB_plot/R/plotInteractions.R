b82ff37bf1df321b60d7c25bb9af1dd155859867
veghp <31794642+veghp@users.noreply.github.com>
2019-04-17 11:38:17 +0100

    Fix color scale for P-values

--- cellphoneDB_plot/R/plotInteractions.R
+++ cellphoneDB_plot/R/plotInteractions.R
@@ -3,7 +3,7 @@ plotInteractions <- function(cpdb.results) {
   cols.use <- c("red", "blue")
   p <- ggplot(data = cpdb.results, mapping = aes(x = Cellpairs,  y = Interactions)) +
     geom_point(mapping = aes(size = Means, color = Pvalues), shape = 15) +
-    scale_color_gradient(low = cols.use[1], high = cols.use[2]) +
+    scale_color_gradient(low = cols.use[1], high = cols.use[2], limits=c(0, 0.05)) +
     theme(axis.text.x = element_text(angle = 90, hjust = 1))
   return(p)
 }
