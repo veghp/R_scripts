0f659e4d2e5f8e41399e12881181ea75f404f968
veghp <31794642+veghp@users.noreply.github.com>
2019-04-25 16:23:15 +0100

    Correct variable name

--- cellphoneDB_plot/R/plotInteractions2.R
+++ cellphoneDB_plot/R/plotInteractions2.R
@@ -1,7 +1,7 @@
 plotInteractions2 <- function(cpdb.results) {
   require("ggplot2")
   cols.use <- c("red", "blue")
-  p <- ggplot(data = cpdb.results.subset, mapping = aes(x = Cellpairs, y = Interactions)) +
+  p <- ggplot(data = cpdb.results, mapping = aes(x = Cellpairs, y = Interactions)) +
     geom_point(mapping = aes(size = log2(Means), color = Pvalues)) +
     scale_color_gradient(low = cols.use[1], high = cols.use[2], limits=c(0, 0.05)) +
     theme(axis.text.x = element_text(angle = 90, hjust = 1), panel.background = element_blank())
