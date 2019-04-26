1e4006dc6b2ba50caeee35e23547ed46c8e9a039
peter <peter@newhuaweiapstaff-10-50-16-3.ncl.ac.uk>
2019-04-26 11:59:56 +0100

    Combined plotting functions

--- cellphoneDB_plot/R/plotInteractions.R
+++ cellphoneDB_plot/R/plotInteractions.R
@@ -1,9 +1,17 @@
-plotInteractions <- function(cpdb.results) {
+plotInteractions3 <- function(cpdb.results, type = 1) {
   require("ggplot2")
   cols.use <- c("red", "blue")
-  p <- ggplot(data = cpdb.results, mapping = aes(x = Cellpairs,  y = Interactions)) +
-    geom_point(mapping = aes(size = Means, color = Pvalues), shape = 15) +
-    scale_color_gradient(low = cols.use[1], high = cols.use[2], limits=c(0, 0.05)) +
+  p <- ggplot(data = cpdb.results, mapping = aes(x = Cellpairs,  y = Interactions))
+  if(type == 1) {
+    p <- p + geom_point(mapping = aes(size = Means, color = Pvalues), shape = 15) +
+    scale_color_gradient(low = cols.use[1], high = cols.use[2], limits = c(0, 0.05)) +
     theme(axis.text.x = element_text(angle = 90, hjust = 1))
-  return(p)
+    return(p)
+  } else if(type == 2) {
+    p <- p + geom_point(mapping = aes(size = log2(Means), color = Pvalues)) +
+      scale_color_gradient(low = cols.use[1], high = cols.use[2], limits = c(0, 0.05)) +
+      theme(axis.text.x = element_text(angle = 90, hjust = 1), panel.background = element_blank())
+    return(p)
+
+  } else { stop("Argument 'type' must be 1 or 2.") }
 }
--- cellphoneDB_plot/R/plotInteractions2.R
+++
@@ -1,9 +0,0 @@
-plotInteractions2 <- function(cpdb.results) {
-  require("ggplot2")
-  cols.use <- c("red", "blue")
-  p <- ggplot(data = cpdb.results, mapping = aes(x = Cellpairs, y = Interactions)) +
-    geom_point(mapping = aes(size = log2(Means), color = Pvalues)) +
-    scale_color_gradient(low = cols.use[1], high = cols.use[2], limits=c(0, 0.05)) +
-    theme(axis.text.x = element_text(angle = 90, hjust = 1), panel.background = element_blank())
-  return(p)
-}
