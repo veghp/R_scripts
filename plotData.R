e01c9523f130f76b95c441c7a81d2eb1aa8e83fa
veghp <31794642+veghp@users.noreply.github.com>
2019-06-19 14:43:09 +0100

    plotData() added

---
+++ plotData.R
@@ -0,0 +1,25 @@
+plotData <- function(data, x, y, z = NULL, line = NULL) {
+  if(is.null(z)) {
+    z.dat <- rep(1, dim(data)[1])
+  } else {
+    z.dat <- data[, z]
+  }
+
+  symbols(x = data[, x], y = data[, y], circles = (sqrt(z.dat / pi)),
+    inches = 1/10, ann = T, bg = "steelblue2", fg = "black",
+    xlim = c(0, max(data[, x])), ylim = c(0, max(data[, y])),
+    xlab = x, ylab = y)
+
+  title(main = paste('Cor =', round(cor(data[, x], data[, y]), 2)))
+
+  if(line == "line") {
+    linm <- lm(data[, y] ~ data[, x])
+    abline(a = linm$coefficients[1], b = linm$coefficients[2], col = "gray50")
+  } else if(line == "spline") {
+    spl <- smooth.spline(data[, x], data[, y], spar = 1)
+    lines(spl, col = "red")
+  }
+
+  saveplot <- recordPlot()
+  return(saveplot)
+}
