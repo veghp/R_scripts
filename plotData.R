a8b0fa7c10b27bd5f9595c7507c7fd54326d7aeb
veghp <31794642+veghp@users.noreply.github.com>
2019-07-07 21:44:56 +0100

    plotData() update

--- plotData.R
+++ plotData.R
@@ -1,22 +1,23 @@
-plotData <- function(data, x, y, z = NULL, line = "", spar = 1, xlim = c(0, max(data[, x]))) {
+plotData <- function(d, x, y, z = NULL, line = F, spline = F, spar = 1, xlim = c(0, max(d[, x]))) {
   if(is.null(z)) {
-    z.dat <- rep(1, dim(data)[1])
+    z.dat <- rep(1, dim(d)[1])
   } else {
-    z.dat <- data[, z]
+    z.dat <- d[, z]
   }
 
-  symbols(x = data[, x], y = data[, y], circles = (sqrt(z.dat / pi)),
+  symbols(x = d[, x], y = d[, y], circles = (sqrt(z.dat / pi)),
     inches = 1/10, ann = T, bg = "steelblue2", fg = "black",
-    xlim = xlim, ylim = c(0, max(data[, y])),
-    xlab = x, ylab = y)
+    xlim = xlim, ylim = c(0, max(d[, y])),
+    xlab = x, ylab = y, las = 1)
 
-  title(main = paste('Cor =', round(cor(data[, x], data[, y]), 2)))
+  title(main = paste('Cor =', round(cor(d[, x], d[, y]), 2)))
 
-  if(line == "line") {
-    linm <- lm(data[, y] ~ data[, x])
+  if(line) {
+    linm <- lm(d[, y] ~ d[, x])
     abline(a = linm$coefficients[1], b = linm$coefficients[2], col = "gray50")
-  } else if(line == "spline") {
-    spl <- smooth.spline(data[, x], data[, y], spar = spar)
+  }
+  if(spline) {
+    spl <- smooth.spline(d[, x], d[, y], spar = spar)
     lines(spl, col = "red")
   }
 
