87e71da8798a6649858362cd39dc17ff2556060c
veghp <31794642+veghp@users.noreply.github.com>
2019-07-13 11:25:45 +0100

    Read V(D)J html results

--- read10xsummary.R
+++ read10xsummary.R
@@ -1,10 +1,11 @@
-read10xsummary <- function(tenx.samples, path = NULL, what = "csv") {
+read10xsummary <- function(tenx.samples, path = NULL, what = "csv", vdj = F) {
   # This function reads 10x cellranger metrics_summary.csv or web_summary.html
   # files into a matrix.
   # tenx.samples: vector of 10x sample names (directories)
   # path: vector of csv or html file paths. Specify if current working dir does
   # not contain sample dirs (tenx.samples), or if the files are somewhere else.
   # what: read "csv" or "html" files
+  # vdj: is the html file from a V(D)J experiment?
   # Functions:
   removeComma <- function(txt) {gsub(",", "", txt, fixed = TRUE)}
 
@@ -45,39 +46,66 @@ read10xsummary <- function(tenx.samples, path = NULL, what = "csv") {
     }
     web_summary.html.files <- path
 
-    to.match <- c(
-      "<td>Number of Reads</td>",
-      "<td>Sequencing Saturation</td>",
-      "<td>Estimated Number of Cells</td>",
-      "<td>Fraction Reads in Cells</td>",
-      "<td>Mean Reads per Cell</td>",
-      "<td>Median Genes per Cell</td>",
-      "<td>Total Genes Detected</td>",
-      "<td>Median UMI Counts per Cell</td>") # will get these from html
-    to.match <- paste(to.match, collapse="|")
+    if(!is.logical(vdj)) {stop("'vdj' must be T or F.")}
 
-    summary.html <- data.frame(row.names = c(
-      "Number.of.Reads",
-      "Sequencing.Saturation",
-      "Estimated.Number.of.Cells",
-      "Fraction.Reads.in.Cells",
-      "Mean.Reads.per.Cell",
-      "Median.Genes.per.Cell",
-      "Total.Genes.Detected",
-      "Median.UMI.Counts.per.Cell"))
+    if(vdj) {
+      to.match <- c( # order must match first appearance in html:
+        "<td>Reads Mapped to Any V\\(D\\)J Gene</td>",
+        "<td>Reads Mapped to TRA</td>",
+        "<td>Reads Mapped to TRB</td>",
+        "<td>Estimated Number of Cells</td>",
+        "<td>Cell Count Confidence</td>",
+        "<td>Mean Read Pairs per Cell</td>",
+        "<td>Mean Used Read Pairs per Cell</td>",
+        "<td>Fraction Reads in Cells</td>",
+        "<td>Number of Read Pairs</td>") # will get these from html
+      to.match <- paste(to.match, collapse="|")
+
+      summary.html <- data.frame(row.names = c(
+        "Reads.Mapped.to.Any.VDJ.Gene",
+        "Reads.Mapped.to.TRA",
+        "Reads.Mapped.to.TRB",
+        "Estimated.Number.of.Cells",
+        "Cell.Count.Confidence",
+        "Mean.Read.Pairs.per.Cell",
+        "Mean.Used.Read.Pairs.per.Cell",
+        "Fraction.Reads.in.Cells",
+        "Number.of.Read.Pairs"))
+
+    } else { # implicit vdj == F
+      to.match <- c( # order must match first appearance in html:
+        "<td>Number of Reads</td>",
+        "<td>Sequencing Saturation</td>",
+        "<td>Estimated Number of Cells</td>",
+        "<td>Fraction Reads in Cells</td>",
+        "<td>Mean Reads per Cell</td>",
+        "<td>Median Genes per Cell</td>",
+        "<td>Total Genes Detected</td>",
+        "<td>Median UMI Counts per Cell</td>") # will get these from html
+      to.match <- paste(to.match, collapse="|")
+
+      summary.html <- data.frame(row.names = c(
+        "Number.of.Reads",
+        "Sequencing.Saturation",
+        "Estimated.Number.of.Cells",
+        "Fraction.Reads.in.Cells",
+        "Mean.Reads.per.Cell",
+        "Median.Genes.per.Cell",
+        "Total.Genes.Detected",
+        "Median.UMI.Counts.per.Cell"))
+    }
 
     for(f in 1:length(web_summary.html.files)) {
 
       command.txt <- paste0(
-        "grep -A 1 -E '", to.match, "' ",
-        web_summary.html.files[f])
+        "grep -A 1 -E '", to.match, "' ", web_summary.html.files[f])
 
       grep.txt <- system(command.txt, wait = T, intern = T)
       grep.txt <- gsub("      <td>", "", grep.txt)
       grep.txt <- gsub("</td>", "", grep.txt)
       grep.txt <- gsub(",", "", grep.txt) # remove junk txt
-
-      to.df <- grep.txt[seq(2, 23, 3)] # every 4th element is a number we need
+      # every 4th element is a number we need, starting from the 2nd:
+      to.df <- grep.txt[seq(2, nrow(summary.html)*3, 3)]
       summary.html[, tenx.samples[f]] <- to.df
     }
 
